6589383dd702644197913faa69ac32553f8eeab2
Aabi-32 <aabiabdilahi@gmail.com>
2025-05-11 15:02:20 +0300

    the main classes commited

---
+++ resource/exercise1/Animal.java
@@ -0,0 +1,6 @@
+package exercise1;
+
+public interface Animal {
+    void eat();
+    void sound();
+}
---
+++ resource/exercise1/Animals.java
@@ -0,0 +1,13 @@
+package exercise1;
+
+public class Animals {
+    public static void main(String[] args) {
+        Animal dog =new Dog();
+        dog.sound();
+        dog.eat();
+        Animal cow =new Cow();
+        cow.sound();
+        cow.eat();
+    }
+
+}
